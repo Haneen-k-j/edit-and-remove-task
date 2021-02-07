2c281877845ebd0de14c3f2abbb051fe12757f0e
unknown <nmnoom97@gmail.com>
2021-02-07 11:36:09 +0300

    this task javascript part  three contain edit function

---
+++ js/dataTable.js
@@ -0,0 +1,79 @@
+var selectedRow = null
+
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+}
+
+function readFormData() {
+    var formData = {};
+    formData["id"] = document.getElementById("id").value;
+    formData["name"] = document.getElementById("name").value;
+    formData["fullName"] = document.getElementById("fullName").value;
+    formData["department"] = document.getElementById("department").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.id;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.name;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.fullName;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.name;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<a onClick="onEdit(this)">   <i class="fas fa-user-edit"></i></a>
+                       <a onClick="onDelete(this)">    <i class="fas fa-trash-alt "></i></a>`;
+}
+
+function resetForm() {
+    document.getElementById("id").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("fullName").value = "";
+    document.getElementById("name").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("id").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("name").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("fullName").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("department").value = selectedRow.cells[3].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.id;
+    selectedRow.cells[1].innerHTML = formData.name;
+    selectedRow.cells[2].innerHTML = formData.fullName;
+    selectedRow.cells[3].innerHTML = formData.department;
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+function validate() {
+    isValid = true;
+    if (document.getElementById("id").value == "") {
+        isValid = false;
+        document.getElementById("fullNameValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
+            document.getElementById("fullNameValidationError").classList.add("hide");
+    }
+    return isValid;
+}
